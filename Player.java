cb4253044fd4d73eb94807e49f80bbb809416131
OkaGouhei <g.oka1975@gmail.com>
2019-08-28 22:08:46 +0900

    修正
    Playerの4-12行のインデントがおかしいです。
    Scannerはstaticでないならコンストラクタで初期化してください。
    HandMarkは小文字ではじめるべきです。

--- Player.java
+++ Player.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 class Player{
-    Scanner scan = new Scanner(System.in);
-        private String name;
-        private int handmark;
-        public void setName(){
-            System.out.println("名前を入力して下さい");
-            String name = scan.next();
-            this.name = name;
-            System.out.println(this.name + "さんですね");
-        }
-        public String getName(){
-        return this.name;
+    Scanner scan ;
+    public Player(){
+        scan = new Scanner(System.in);
+    }
+
+    private String name;
+    private int handmark;
+    public void setName(){
+        System.out.println("名前を入力して下さい");
+        String name = scan.next();
+        this.name = name;
+        System.out.println(this.name + "さんですね");
+    }
+    public String getName(){
+    return this.name;
     }
     public void setHandmark(){
         System.out.println(this.name + "さんの数字を入力してください。グーが1、チョキが2、パーが3です。");
@@ -19,8 +23,8 @@ class Player{
             System.out.println("もういちど入力してください");
             number = scan.nextInt();
         }
-        String[] HandMark = { "" , "グー" , "チョキ" , "パー" };
-        System.out.println(this.name + "さんの出した手は" + HandMark[number] + "ですね");
+        String[] jankenMark = { "" , "グー" , "チョキ" , "パー" };
+        System.out.println(this.name + "さんの出した手は" + jankenMark[number] + "ですね");
         this.handmark = number;
     }
     public int getHandmark(){
